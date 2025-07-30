820930ed33200ff79785beb332aec011e3dcf02c
Kresa12 <aurakresa12@gmail.com>
2025-07-30 22:24:08 +0700

    perf: store modelId and apiKey in Firestore
    do this to make it easier to change model

--- app/src/main/java/com/esa/graffai/data/repository/RoboflowRepository.kt
+++ app/src/main/java/com/esa/graffai/data/repository/RoboflowRepository.kt
@@ -2,16 +2,23 @@ package com.esa.graffai.data.repository
 
 import com.esa.graffai.data.model.PredictedResponeseModel
 import com.esa.graffai.data.remote.api.APIService
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
 import okhttp3.MultipartBody
 import javax.inject.Inject
 
 class RoboflowRepository @Inject constructor(
-    private val apiService : APIService
+    private val apiService: APIService,
+    private val firestore: FirebaseFirestore
 ) {
 
-    suspend fun classyfyImage(file : MultipartBody.Part): PredictedResponeseModel{
-        val modelId = "masterpiece-nograffiti-tags-throwups-9j0tt/1"
-        val apiKey = "pTa8NoUR9zdDsE1h4lc1"
+    suspend fun classyfyImage(file: MultipartBody.Part): PredictedResponeseModel {
+        val snapshot = firestore.collection("roboflow_config").document("LivzQbViX6Zo8A8qhzbD").get().await()
+
+        val modelId = snapshot.getString("modelId") ?: error("Model ID missing")
+        val apiKey = snapshot.getString("apiKey") ?: error("API key missing")
+
         return apiService.classifyImage(modelId = modelId, apiKey = apiKey, file = file)
     }
+
 }
