8b1a043757ba32e72d4b3d73b01d859ec86b5bad
Kresa12 <aurakresa12@gmail.com>
2025-07-30 15:49:29 +0700

    fix: roboflowViewModel implement helper for convert uri to multipart

--- app/src/main/java/com/esa/graffai/viewmodel/RoboflowViewModel.kt
+++ app/src/main/java/com/esa/graffai/viewmodel/RoboflowViewModel.kt
@@ -1,16 +1,15 @@
 package com.esa.graffai.viewmodel
 
+import android.content.Context
+import android.net.Uri
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.esa.graffai.data.repository.RoboflowRepository
+import com.esa.graffai.utils.FileUtils
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
-import okhttp3.MediaType.Companion.toMediaTypeOrNull
-import okhttp3.MultipartBody
-import okhttp3.RequestBody.Companion.asRequestBody
-import java.io.File
 import javax.inject.Inject
 
 @HiltViewModel
@@ -21,17 +20,20 @@ class RoboflowViewModel @Inject constructor(
     private val _result = MutableLiveData<String>()
     val result: LiveData<String> = _result
 
-    fun classifyImage(imageFile: File) {
+    fun classifyImageFromUri(context: Context, uri: Uri) {
         viewModelScope.launch {
-            val requestFile = imageFile.asRequestBody("image/*".toMediaTypeOrNull())
-            val multipart = MultipartBody.Part.createFormData("file", imageFile.name, requestFile)
-
-            try {
-                val response = repository.classyfyImage(multipart)
-                _result.value = response.toString()
-            } catch (e: Exception) {
-                _result.value = "Error: ${e.message}"
+            val multipart = FileUtils.uriToMultipart(context, uri)
+            if (multipart != null) {
+                try {
+                    val response = repository.classyfyImage(multipart)
+                    _result.value = response.toString()
+                } catch (e: Exception) {
+                    _result.value = "Error: ${e.message}"
+                }
+            } else {
+                _result.value = "Gagal membaca gambar dari URI"
             }
         }
     }
+
 }
