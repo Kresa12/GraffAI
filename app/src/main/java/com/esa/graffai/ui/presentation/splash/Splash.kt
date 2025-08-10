c869e6648fbe67ca1f3bd86456ffec470dcbabf7
Kresa12 <aurakresa12@gmail.com>
2025-08-10 21:09:40 +0700

    feat: WIP make splash screen

---
+++ app/src/main/java/com/esa/graffai/ui/presentation/splash/Splash.kt
@@ -0,0 +1,21 @@
+package com.esa.graffai.ui.presentation.splash
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Spash(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
+        Image()
+    }
+}
