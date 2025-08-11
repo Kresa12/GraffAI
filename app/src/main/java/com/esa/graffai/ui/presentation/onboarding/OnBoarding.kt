b8d0f75a989240b301678bcbf32d631829c43828
Kresa12 <aurakresa12@gmail.com>
2025-08-11 21:08:36 +0700

    feat: WIP OnBoarding Screen

---
+++ app/src/main/java/com/esa/graffai/ui/presentation/onboarding/OnBoarding.kt
@@ -0,0 +1,18 @@
+package com.esa.graffai.ui.presentation.onboarding
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+
+@Composable
+fun OnBoarding(
+    modifier: Modifier = Modifier,
+    navController: NavController
+) {
+    Column(
+        modifier = modifier,
+    ) {
+
+    }
+}
